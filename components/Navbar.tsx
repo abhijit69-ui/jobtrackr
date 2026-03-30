099e90ede030a04f2add4cb7bbbab4edac49772a
Abhijit Nath <abhijitnath1a1@gmail.com>
2026-03-30 13:48:25 +0530

    feat: implement authentication with better-auth and create sign-in/sign-up pages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { CodeXmlIcon } from 'lucide-react';
 import Link from 'next/link';
 import { Button } from './ui/button';
+import { getSession } from '@/lib/auth/auth';
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await getSession();
   return (
     <nav className='border-b border-gray-200 bg-white'>
       <div className='container mx-auto flex justify-between h-16 items-center px-4'>
@@ -15,19 +17,34 @@ export default function Navbar() {
         </Link>
 
         <div className='flex items-center gap-4'>
-          <Link href='/sign-in'>
-            <Button
-              variant='ghost'
-              className='text-gray-700 hover:text-black cursor-pointer'
-            >
-              Log In
-            </Button>
-          </Link>
-          <Link href='/sign-up'>
-            <Button className='hover:bg-primary/90 cursor-pointer'>
-              Start for free
-            </Button>
-          </Link>
+          {session?.user ? (
+            <>
+              <Link href={'/dashboard'}>
+                <Button
+                  variant={'ghost'}
+                  className='text-gray-700 hover:text-black cursor-pointer'
+                >
+                  Dashboard
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href='/sign-in'>
+                <Button
+                  variant='ghost'
+                  className='text-gray-700 hover:text-black cursor-pointer'
+                >
+                  Log In
+                </Button>
+              </Link>
+              <Link href='/sign-up'>
+                <Button className='hover:bg-primary/90 cursor-pointer'>
+                  Start for free
+                </Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
